b766ec0f621abdc7ccfdf3cabc6bec5090fb3b39
matsiyan <48468303+matsiyan@users.noreply.github.com>
2019-06-21 00:04:52 -0700

    Euphini and Lower
    Euphini Expanse Inversion images and entities locations corrected.
    Revised draft of lower arc gate net image

---
+++ Nav/sectors/Euphini Expanse/invert.bat
@@ -0,0 +1,17 @@
+echo off
+call swap 1 5
+pause
+call swap 2 4
+pause
+call swap 6 10
+pause
+call swap 7 9
+pause
+call swap 11 15
+pause
+call swap 12 14
+pause
+call swap 16 20
+pause
+call swap 17 19
+pause
---
+++ Nav/sectors/Euphini Expanse/swap.bat
@@ -0,0 +1,4 @@
+echo Swapping %1 and %2
+ren %1.png t
+ren %2.png %1.png
+ren t %2.png
